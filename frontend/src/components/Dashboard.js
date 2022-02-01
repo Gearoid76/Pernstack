50716244bc0a065afac7faa37bce2779ee62a6c4
Gearóid O'Ceallacháin <gearoidoceallachain@Gwen.local>
2022-02-01 15:16:09 +0200

    adding frontend hellohello.text and server

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import Header from "./Header";
+
+const Dashboard = ({ setAuth }) => {
+
+    const [name,setName] = useState("")
+
+    async function getName() {
+        try {
+         const response = await fetch("http://localhost:4000/dashboard/",  {
+             method: "GET",
+             headers: { token: localStorage.token }
+         });
+
+         const parseRes = await response.json()
+
+         setName(parseRes.username);
+            
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    const logout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        setAuth(false);
+        toast.success("Logged out successfully");
+    }
+
+    useEffect(() => {
+        getName()
+    },[]);
+
+    return (
+        <Fragment>
+            <Header />
+            <h1>Dashboard!! {name}</h1>
+            <button className="btn btn-primary" onClick={e => logout(e)}>Logout</button>
+        </Fragment>
+    );
+};
+
+export default Dashboard;
+
