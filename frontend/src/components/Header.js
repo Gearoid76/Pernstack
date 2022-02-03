09fd776e9dc538e30ad289a3f63d9319c5fbd891
Gearoid <gocealla@gmail.com>
2022-02-03 15:53:04 +0200

    uploading and fixing the slack sign

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className="container"> 
+        <div className="container text-center"> 
             <img  src="https://a.slack-edge.com/bv1-9/slack_logo-ebd02d1.svg" width="50%" padding="30px" alt ="company-logo go.inc"></img>
         </div>
             
